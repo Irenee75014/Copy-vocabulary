8e248fae89623f9202bed6e2676ad3df314b5033
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-25 11:37:43 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,27 @@
-streamlit as st 
+import streamlit as st 
 import pandas as pd
-voc=pd.nead_CSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vSQ6MdH76cliOGXJgp2c3o7iEUQ9Z8bGhsCgc5H_DxAeZaB_52mdBCjASh-libSTDw_3pm_H5_QqtSg/pub?output=csv')
-st.dataframe(voc)
+import numpy as np
+voc = pd.read_csv(' https://docs.google.com/spreadsheets/d/e/2PACX-1vSQ6MdH76cliOGXJgp2c3o7iEUQ9Z8bGhsCgc5H_DxAeZaB_52mdBCjASh-libSTDw_3pm_H5_QqtSg/pub?output=csv ')
+l = voc.shape[0]
+indices = np.random.choice(l, size=4, replace=False)
+j = np.random.choice(indices)
+word_fr=voc['Définition'].values[j]
+st.write('Traduis: '+word_fr)
+
+def is_correct(i, j):
+  if i==j:
+    st.write("Bien joué !")
+  else:
+    st.write("Perdu !")
+
+col1, col2 = st.columns(2) 
+with col1:
+    for i in range(2):
+        st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+with col2:
+    for i in range(2,4):
+        st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+
+
+#st.button("Seconday button")  # st.button default type is secondary
+#st.button("Primary button", type="primary")
